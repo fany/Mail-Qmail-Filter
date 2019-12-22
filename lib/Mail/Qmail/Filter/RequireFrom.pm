93bc4f8d1c72ad7a9e5d7c496420065c42b662c7
Martin H. Sluka <mensa@martin.sluka.de>
2019-12-23 00:47:17 +0100

    added lowercase_from option for RequireFrom filter

--- lib/Mail/Qmail/Filter/RequireFrom.pm
+++ lib/Mail/Qmail/Filter/RequireFrom.pm
@@ -13,14 +13,19 @@ use Mo qw(coerce default required);
 extends 'Mail::Qmail::Filter';
 
 has 'allowed_addresses' => coerce => \&addresses2hash, required => 1;
-has 'reject_text'       => sub { "<@_> not allowed as RFC5322.From" };
+has 'lowercase_from';
+has 'reject_text' => sub { "<@_> not allowed as RFC5322.From" };
 
 sub filter {
     my $self                = shift;
     my $header_from_address = '';
     if ( my $header_from = $self->message->header_from ) {
         $header_from_address = $header_from->address;
-        return if match_addr( $self->allowed_addresses, $header_from_address );
+        return
+          if match_addr( $self->allowed_addresses,
+            $self->lowercase_from
+            ? lc $header_from_address
+            : $header_from_address );
     }
     $self->reject( $self->reject_text, $header_from_address );
 }
